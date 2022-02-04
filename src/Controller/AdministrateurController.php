e639f56ad9455268bfe6044c6d7221d1d4184110
Meryaaaaam <meryam.chouaiti@smartegy.ca>
2022-02-05 00:10:14 +0100

    administration

--- src/Controller/AdministrateurController.php
+++ src/Controller/AdministrateurController.php
@@ -29,11 +29,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-/**
- * Require ROLE_ADMIN for all the actions of this controller
- *
- * @IsGranted("IS_AUTHENTICATED_FULLY")
- */
+
 class AdministrateurController extends AbstractController  
 {
     public function __construct(AdministrateurRepository $administrateurRepository,
