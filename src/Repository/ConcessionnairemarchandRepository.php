25a4c586c242fa47560d18ce462f61833b4a817d
Meryaaaaam <meryam.chouaiti@smartegy.ca>
2022-03-03 17:49:36 +0100

    Update ConcessionnairemarchandRepository.php

--- src/Repository/ConcessionnairemarchandRepository.php
+++ src/Repository/ConcessionnairemarchandRepository.php
@@ -61,7 +61,7 @@ class ConcessionnairemarchandRepository extends ServiceEntityRepository
         return $this->createQueryBuilder('c')
             ->addSelect('c')
             ->innerjoin('c.marchand', 'm')
-            ->where('m.id = c.id')
+            ->where('m.id = :val')
             ->setParameter('val', $value)
             ->getQuery()
             ->getOneOrNullResult()
